4ebc7a6270ace263ef8224a72480ea2c72ecd11a
Amit Gupta <42681478+Amit-Gupta-Code@users.noreply.github.com>
2026-08-31 12:33:23 +0530

    updated

--- api/visitors.ts
+++ api/visitors.ts
@@ -1,7 +1,7 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { handleVisitors } from "../server/visitors";
 
-export const config = { runtime: "nodejs20.x" };
+export const config = { runtime: "nodejs" };
 
 export default async function handler(req: IncomingMessage, res: ServerResponse): Promise<void> {
   try {
